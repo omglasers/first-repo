85f7d31c9d24696153f014a10091bf178f603718
Marian Petre <marian5234@gmail.com>
2015-07-23 13:44:39 +0300

    tutorial part 3 done

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,9 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls'))
-    
+	url(r'^polls/', include('polls.urls', namespace="polls")),
+	url(r'^admin/', include(admin.site.urls)),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
