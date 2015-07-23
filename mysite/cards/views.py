e2e859ea61507766fda247db1081f14bd8605d13
Marian Petre <marian5234@gmail.com>
2015-07-23 16:21:30 +0300

    WIP card view

--- mysite/cards/views.py
+++ mysite/cards/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from .models import Card
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+	return HttpResponse("Hello, world. You're at the cards index.")
+
+class IndexView(generic.ListView):
+	template_name = 'cards/index.html'
+	context_object_name = 'latest_card_list'
+	def get_queryset(self):
+		"""Return the last five published cards."""
+		return Card.objects.order_by('name')[:5]
+
+
+class DetailView(generic.DetailView):
+	model = Card
+	template_name = 'cards/detail.html'
