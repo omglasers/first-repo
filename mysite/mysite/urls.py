4989d5748db4f232bafa618848f1497237488533
Marian Petre <marian5234@gmail.com>
2015-07-23 15:23:08 +0300

    form added

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-	url(r'^polls/', include('polls.urls', namespace="polls")),
-	url(r'^admin/', include(admin.site.urls)),
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^admin/', include(admin.site.urls)),
+]
